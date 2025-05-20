e651d94d456c67ec298a5f2c911c779a1511b8a4
trixll4174 <trixll4174@masseyhigh.school.nz>
2025-05-20 12:41:19 +1200

    Both basic programs from video one and two

---
+++ P_02_VARIABLES.py
@@ -0,0 +1,13 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_number = 7
+num_string = "5"
+
+print(fav_number * 2)
+print(num_string * 5)
+
+
